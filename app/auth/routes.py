74f4c76b483e435d4e1994e76f969ea9956936b1
anastasiiaKorobka <anastasiia.korobka@allegro.com>
2024-10-21 17:15:11 +0200

    typeofreaders.txt

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, current_user
 from app import db
 from app.auth import bp
-from app.auth.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 @bp.route('/register', methods=['GET', 'POST'])
